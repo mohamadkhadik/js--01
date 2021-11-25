ba1b291effff0daa048dd73a5c0a75d1962e2c4e
Khadik86 <mohamadkhadik7@gmail.com>
2021-11-25 10:29:19 +0700

    frist commit

---
+++ index.js
@@ -0,0 +1,61 @@
+// Variabel
+
+// var nama = "mohamad khadik"
+// // console.log (nama);
+// // prompt(nama);
+// // alert(nama);
+
+// let a =10;
+// // console.log (a)
+
+// // keyword const
+// const hobby ="catur"
+// console.log(hobby)
+
+// // opertor aritmatika(+)
+// let c = 15
+// let b = 4
+// console.log (a+b)
+
+// // opertor aritmatika(%)
+// // sissa bagi pada pembagian 
+// let x = 20 
+// let y = 2
+// console.log (x%y)
+
+// // opertor aritmatika(=) if else
+// let = 10 
+// if (a == "10") {
+//     console.log ("a adalah benar")
+// }else{
+//     console.log ("a adalah salah")
+// }
+
+
+// else if 
+// var angka = prompt ("masukan angka")
+
+// if (angka % 2 == 0) {
+//     alert(angka+ " yang anda msukan bilangan genap")
+// } else if(angka % 2 == 1){
+//     alert(angka+ " yang anda msukan bilangan genjil")
+// }else{
+//     alert("yang anda masukan bukan angka")
+// }
+// var usia = prompt ("masukan angka")
+
+// if (usia > 0 && usia < 5) {
+//     alert(usia+ " anda belita")
+// } else if(usia >= 5 && usia < 12){
+//     alert(usia+ " anda anak anak ")
+// } else if(usia > 12 && usia <= 20){
+//     alert(usia+ " anda remaja")
+// }else if(usia > 21 && usia <= 45){
+//     alert(usia+ " anda dewasa")
+// } else if(usia > 45 && usia <= 46){
+//     alert(usia+ " anda lansia")
+// }else{
+//     alert("rehat")
+// }
+
+
---
+++ tugas.js
@@ -0,0 +1,32 @@
+var nilaiRaya = 80;
+
+
+if(nilaiRaya >= 80){ 
+  console.log("Nilai"+nilaiRaya+"= A"); 
+}else if(nilaiRaya >= 70 && nilaiRaya < 80){ 
+    console.log("Nilai"+nilaiRaya+"= B"); 
+}else if(nilaiRaya >= 60 && nilaiRaya < 70){ 
+    console.log("Nilai"+nilaiRaya+"= C");
+}else if(nilaiRaya >= 50 && nilaiRaya < 60){ 
+    console.log("Nilai"+nilaiRaya+"= D") 
+}else if(nilaiRaya < 50){
+    console.log("Nilai"+nilaiRaya+"= E") 
+}else{
+    console.log("tidak ada Predikatnya")
+}
+
+var nilaiGenta = 50
+
+if(nilaiGenta >= 80){ 
+    console.log("Nilai"+nilaiGenta+"= A"); 
+  }else if(nilaiGenta >= 70 && nilaiGenta < 80){ 
+      console.log("Nilai"+nilaiGenta+"= B"); 
+  }else if(nilaiGenta >= 60 && nilaiGenta < 70){ 
+      console.log("Nilai"+nilaiGenta+"= C");
+  }else if(nilaiGenta >= 50 && nilaiGenta < 60){ 
+      console.log("Nilai"+nilaiGenta+"= D") 
+  }else if(nilaiGenta < 50){
+      console.log("Nilai"+nilaiGenta+"= E") 
+  }else{
+    console.log("tidak ada Predikatnya")
+}
